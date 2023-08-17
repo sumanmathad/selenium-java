753802f65a637d95b282a7cb1ced21a867269c09
suman_mathad <suman.mathad@gmail.com>
2023-08-17 12:16:03 +0530

    Selenium java test updates

--- src/test/java/com/BrowserActions/WebDriverBasics.java
+++ src/test/java/com/BrowserActions/WebDriverBasics.java
@@ -29,15 +29,37 @@ public class WebDriverBasics {
         driver.findElement(By.cssSelector(".card-body [href='web-form.html']")).click();
         driver.findElement(By.cssSelector("#my-text-id")).sendKeys("This is a string text.");
         driver.findElement(By.cssSelector("input[name='my-password']")).sendKeys("Pass12345#");
-        driver.findElement(By.cssSelector("label textarea")).sendKeys("test 1" +" test2");
+        driver.findElement(By.cssSelector("label textarea")).sendKeys("text area content");
 
 
     }
+    @Test
+    public void navigation(){
+        driver.get(bonigarciaTestPage);
+        Assert.assertTrue(driver.getTitle().equals("Hands-On Selenium WebDriver with Java"));
+
+        driver.findElement(By.xpath("//a[text()='Navigation']")).click();
+        Assert.assertTrue(driver.findElement(By.cssSelector("body p")).getText().startsWith("Lorem ipsum"));
+
+        driver.findElement(By.cssSelector(".page-item.active + li")).click();
+        // adjacent combinator locator. It selects the element that is immediately preceded by the former element.
+        Assert.assertTrue(driver.findElement(By.cssSelector("body p")).getText().contains("commodo consequat"));
+
+        driver.findElement(By.xpath("//a[text()='Next']")).click();
+        Assert.assertTrue(driver.findElement(By.cssSelector("body p")).getText().startsWith("Excepteur"));
+
+    }
+
+
+
+
+
 
 
     @AfterTest
     public void teardown(){
         //driver.quit();
+        //code is commented sometimes to verify the output behaviour after test run completion.
     }
 
 
