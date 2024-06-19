a534ad6e84745ab25f1e71c43504e710c8587170
suman.mathad <suman.mathad@oracle.com>
2024-06-19 21:03:58 +0530

    updated Selenium latest version and Webdriver method syntax

--- src/test/java/com/BrowserActions/WebDriverBasics.java
+++ src/test/java/com/BrowserActions/WebDriverBasics.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.locators.RelativeLocator;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
@@ -26,7 +27,7 @@ public class WebDriverBasics {
 
     @BeforeTest
         public void setUp(){
-        WebDriver browserrdriver = WebDriverManager.chromedriver().avoidShutdownHook().create();
+        WebDriver browserrdriver = new ChromeDriver();
         this.driver=browserrdriver;
     }
 
